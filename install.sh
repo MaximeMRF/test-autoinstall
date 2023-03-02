2b6ece1d9419ea6cdb179fad3639661f926826b5
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2023-03-02 09:53:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 # Get the docker-compose file
 curl -o docker-compose.yml https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/docker-compose.yml
+
+# Run the docker-compose file in background
+docker-compose up -d
