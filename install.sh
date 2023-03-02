d9970fdb2a2e42dff494ecdd7b95c3d0638f977f
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2023-03-02 11:09:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,14 @@
 # Get the .env file
 curl -o .env https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/.env.install
 
+# Get the conf file for warpfleet
+curl -O https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/conf.json
+
+# Get the dockerfile for warpfleet
+curl -O https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/dockerfile-wfs
+
 # Get the docker-compose file
-curl -o docker-compose.yml https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/docker-compose.yml
+curl -O https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/docker-compose.yml
 
 # Run the docker-compose file in background
 docker-compose up -d
