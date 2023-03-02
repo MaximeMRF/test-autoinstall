c696a6272aee65391e93b3804e47ff6134cb4229
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2023-03-02 10:51:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# Installation script for Hexa-Data
+
+# curl -sSL https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/install.sh | bash
+
+# Get the .env file
+curl -o .env https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/.env.install
+
 # Get the docker-compose file
 curl -o docker-compose.yml https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/docker-compose.yml
 
